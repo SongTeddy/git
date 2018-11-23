7cbe331b6b1051cbe5f65ab53d4d79345e75cd4b
songTeddy <sjs977@naver.com>
2018-11-23 16:50:34 +0900

    '하이'

--- project/src/exam/HelloExam.java
+++ project/src/exam/HelloExam.java
@@ -6,7 +6,7 @@ public class HelloExam {
 		System.out.println("hello it's me~~");
 
 		System.out.println("종세");
-
+		System.out.println("신스텔라");
 		System.out.println("hello it's me~~ ~~~");
 		System.out.println("hello it's me~~ ~~~");
 		System.out.println("hello it's me~~ ~~~");
