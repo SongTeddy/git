d966efc42c69ac722437af6f179580b7db9c28a9
stellar <stellar1111@naver.com>
2018-11-23 16:54:31 +0900

    브랜치는 어디죠

--- project/src/exam/HelloExam.java
+++ project/src/exam/HelloExam.java
@@ -12,11 +12,9 @@ public class HelloExam {
 		System.out.println("종세");
 		System.out.println("신스텔라");
 		System.out.println("스프링 짜증");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
-
+		System.out.println("짜증쓰");
+		System.out.println("짜증쓰");
+		System.out.println("짜증쓰");
+		System.out.println("짜증쓰");
 	}
 }
