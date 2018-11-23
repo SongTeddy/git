3305c3b2bd382062d48a17328d35770e29b4f817
stellar <stellar1111@naver.com>
2018-11-23 16:46:26 +0900

    appapa

--- project/src/exam/HelloExam.java
+++ project/src/exam/HelloExam.java
@@ -3,11 +3,7 @@ package exam;
 public class HelloExam {
 
 	public static void main(String[] args) {
-		System.out.println("hello it's me~~");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
-		System.out.println("hello it's me~~ ~~~");
+
+		System.out.println("bye bye~ ~~~");
 	}
 }
